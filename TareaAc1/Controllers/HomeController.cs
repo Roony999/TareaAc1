f0e0b1491f826963101aacb8c45654cd061e3463
BrocoTech <BrocoTech@LAPTOP-DQDLCFQP>
2023-09-28 15:12:21 -0600

    Agregar archivos de proyecto.

---
+++ TareaAc1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TareaAc1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult MiPerfil()
+        {
+            return View();
+        }
+    }
+
+    
+}
